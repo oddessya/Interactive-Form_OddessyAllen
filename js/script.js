4aaa076ffbca6233ef37fa55cacb3ba51d654dff
oddessya <oddessya@gmail.com>
2022-07-18 09:39:07 -0500

    Job Role Section
    The "Job Role" section has an <input type="text"> field where users can enter a custom job role. If the user selects "Other" in the "Job Role" drop down menu, they can enter info into the "Other job role" text field. But this field should be hidden by default and only displayed once users select "Other" in the drop down menu, and be hidden if the user selects any other option.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 //When the page loads, the text input type is in focus
 const username = document.getElementById('name');
 username.focus()
+/*
+    Job role Section
+    
+    When the "Other" option is selected/deselected from "Job Role" menu,
+    the "Other job role" field is visible/hidden on the page.
+*/
+const otherJobRole = document.querySelector('#other-job-role');
+otherJobRole.style.display = 'none';
+
+title.addEventListener('change', (e) => {
+    if (e.target.value === 'other'){
+        otherJobRole.style.display = 'initial';
+    } else {
+        otherJobRole.style.display = 'none';
+    }
+});
